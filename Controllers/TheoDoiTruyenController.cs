20eef4431be5ac00ad10bbf156df4a4c1c589b0c
Hanniel1102 <hanbang.tuyet0@gmail.com>
2025-05-02 00:34:57 +0700

    update

---
+++ Controllers/TheoDoiTruyenController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Web_truyen.App_Start;
+using Web_truyen.Models;
+
+namespace Web_truyen.Controllers
+{
+    [RoleUser]
+    public class TheoDoiTruyenController : Controller
+    {
+        Web_TruyenEntities db = new Web_TruyenEntities();
+        // GET: TheoDoiTruyen
+        [HttpPost]
+        public ActionResult AddThuvien(int truyenId) 
+        {
+            var userId = SessionConfig.GetUser().userId;
+          
+            var exists = db.TheoDoi_Truyen.FirstOrDefault(t => t.truyenId == truyenId && t.userId == (int)userId);
+            if (exists == null)
+            {
+                db.TheoDoi_Truyen.Add(new TheoDoi_Truyen
+                {
+                    truyenId = truyenId,
+                    userId = (int)userId,
+                    NgayTheoDoi = DateTime.Now
+                });
+                db.SaveChanges();
+            }
+
+            return RedirectToAction("ChiTiet", "Truyen", new { area = "Admin", id = truyenId });
+        }
+    }
+}
